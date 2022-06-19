761b1c355e773ea905f9c5a4e470ed7fc1d07047
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-19 17:23:53 +0800

    to allow swagger to work

--- src/main/java/com/in28minutes/rest/webservices/version/VersioningController.java
+++ src/main/java/com/in28minutes/rest/webservices/version/VersioningController.java
@@ -3,7 +3,7 @@ package com.in28minutes.rest.webservices.version;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class VersioningController {
 
 	@GetMapping("/personv1")
