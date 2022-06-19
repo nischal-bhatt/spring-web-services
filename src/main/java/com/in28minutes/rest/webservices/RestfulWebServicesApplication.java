2c26a4b48a94413a26ec04a3b2902858e5a3f26b
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-19 16:20:35 +0800

    swagger documentation

--- src/main/java/com/in28minutes/rest/webservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/RestfulWebServicesApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
+	//http://localhost:8080/swagger-ui/index.html
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
